e79174b47b77edd10f8363356467ffaee1ec29a9
zhanghuazheng <18911341753@163.com>
2020-12-11 23:40:42 +0800

    1.增加hashMap常见的面试题，如底层数据结构、hash运算和寻址运算，数组扩容等问题

---
+++ java-base/src/main/java/com/huazheng/test/TestHashSource.java
@@ -0,0 +1,14 @@
+package com.huazheng.test;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashSource {
+    @Test
+    public void test1(){
+        Map<String, String> map = new HashMap<>();
+        map.put("zhz","zhanghuazheng");
+    }
+}
